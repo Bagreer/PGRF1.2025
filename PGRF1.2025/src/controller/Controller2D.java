350035cafc803965c1fb5e3e3270b66109506220
prausmi1 <prausmi1@uhk.cz>
2025-11-14 19:29:24 +0100

    functional changes

--- PGRF1.2025/src/controller/Controller2D.java
+++ PGRF1.2025/src/controller/Controller2D.java
@@ -7,6 +7,7 @@ import fill.SeedFill;
 import model.Line;
 import model.Point;
 import model.Polygon;
+import model.Rectangle;
 import rasterize.*;
 import view.Panel;
 
@@ -37,8 +38,13 @@ public class Controller2D {
 
     private Clipper clipper;
     private Polygon polygonClipper = new Polygon();
+    private Polygon polygonToDraw = new Polygon();
+    List<Point> clippedPoints = new ArrayList<>();
 
 
+    private Rectangle rectangle = new Rectangle();
+    private ArrayList<Point> rPoints = new ArrayList<>(3);
+    private boolean rectangleDrawn = false;
 
     /**
      * třída na ovládání a zobrazování grafických blbostí.
@@ -67,10 +73,8 @@ public class Controller2D {
 
         // seedFill vykreslení
         if (seedFillerStartPoint != null) {
-            seedFiller = new SeedFill(panel.getRaster(), seedFillerStartPoint.getX(), seedFillerStartPoint.getY(), 0x00ff00);
             seedFiller.fill();
         }
-
 
         // TODO vytvořit polygon pomoci pointu (nejlepe pracovat s Listem, ale pokud to rozbije projekt tak pouzit new ArrayList<clipped points>)
 
@@ -80,14 +84,12 @@ public class Controller2D {
         polygonRasterizer.rasterize(polygonClipper);
         LineRasterizer.setColor(0xffffff);
 
-        // --- VOLÁNÍ OŘEZÁVÁNÍ A VYKRESLENÍ VÝSLEDKU ---
-
         // TODO provedu ořezání - Používám Clipper.clip()
-        List<Point> clippedPoints = clipper.clip(polygon.getPoints(), polygonClipper.getPoints());
+        clippedPoints = clipper.clip(polygon.getPoints(), polygonClipper.getPoints());
 
         // TODO vytvořit polygon pomoci pointu
 
-        Polygon polygonToDraw = new Polygon();
+        polygonToDraw = new Polygon();
         for (Point p : clippedPoints) {
             // TODO vznikne novy seznam pointu > vyplním výsledek ořezání pomocí scanLine
             polygonToDraw.addPoint(p);
@@ -99,13 +101,16 @@ public class Controller2D {
                     panel.getRaster(),
                     LineRasterizer
             );
+//            scanLineFiller.fill();
+//            polygonRasterizer.rasterize(polygonToDraw);
         }
         // TODO vyplním výsledek ořezání pomocí scanLine
-        scanLineFiller.fill();
-        polygonRasterizer.rasterize(polygonToDraw);
+
+        polygonRasterizer.rasterize(rectangle);
+        rectangleDrawn = true;
+
         panel.repaint();
     }
-
 
     /**
      * inicializace listeneru (mouse, key listeners)
@@ -119,56 +124,79 @@ public class Controller2D {
                 y1 = e.getY();
                 System.out.println(x1 + " " + y1);
 
-                if (e.getButton() == MouseEvent.BUTTON3) {
+                if (e.getButton() == MouseEvent.BUTTON2) {
                     seedFillerStartPoint = new Point(e.getX(), e.getY());
+                    seedFiller = new SeedFill(panel.getRaster(), seedFillerStartPoint.getX(), seedFillerStartPoint.getY(), 0xffffff);
+                    seedFiller.fill();
+                } else if (e.getButton() == MouseEvent.BUTTON1) {  // Polygon
+                    // Fixace bodu na aktuální pozici kurzoru (currentX/currentY)
+                    polygon.addPoint(new Point(currentX, currentY));
+                    drawScene();
+                } else if (e.getButton() == MouseEvent.BUTTON3) { // Clipper Polygon
+                    polygonClipper.addPoint(new Point(currentX, currentY));
+                    drawScene();
                 }
-
             }
 
             public void mouseReleased(MouseEvent e) {
-                int tempX2 = e.getX();
-                int tempY2 = e.getY();
-
-                // Používáme atribut 'shiftPressed'
-                if (shiftPressed) {
-                    Point aligned = alignCoordinates(x1, y1, tempX2, tempY2);
-                    x2 = aligned.getX();
-                    y2 = aligned.getY();
-                } else {
-                    x2 = tempX2;
-                    y2 = tempY2;
-                }
-
-                System.out.println(x2 + " " + y2);
-                lines.add(new Line(x1, y1, x2, y2));
-                drawScene();
+//                int tempX2 = e.getX();
+//                int tempY2 = e.getY();
+//
+//                // Používáme atribut 'shiftPressed'
+//                if (shiftPressed) {
+//                    Point aligned = alignCoordinates(x1, y1, tempX2, tempY2);
+//                    x2 = aligned.getX();
+//                    y2 = aligned.getY();
+//                } else {
+//                    x2 = tempX2;
+//                    y2 = tempY2;
+//                }
+//
+//                System.out.println(x2 + " " + y2);
+//                lines.add(new Line(x1, y1, x2, y2));
+//                drawScene();
             }
+
         });
 
         panel.addMouseMotionListener(new MouseMotionAdapter() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-
-                int tempX2 = e.getX();
-                int tempY2 = e.getY();
-
-                // Používáme atribut 'shiftPressed'
-                if (shiftPressed) {
-                    Point aligned = alignCoordinates(x1, y1, tempX2, tempY2);
-                    tempX2 = aligned.getX();
-                    tempY2 = aligned.getY();
-                }
-
-                panel.getRaster().clear();
-                LineRasterizer.rasterize(x1, y1, tempX2, tempY2);
-                panel.repaint();
-            }
+//            @Override
+//            public void mouseDragged(MouseEvent e) {
+//
+//                int tempX2 = e.getX();
+//                int tempY2 = e.getY();
+//
+//                if (shiftPressed) {
+//                    Point aligned = alignCoordinates(x1, y1, tempX2, tempY2);
+//                    tempX2 = aligned.getX();
+//                    tempY2 = aligned.getY();
+//                }
+//
+////                panel.getRaster().clear();
+////                LineRasterizer.rasterize(x1, y1, tempX2, tempY2);
+//                panel.repaint();
+//            }
 
             @Override
             public void mouseMoved(MouseEvent e) {
                 currentX = e.getX();
                 currentY = e.getY();
-                //System.out.println("current x:" + currentX + "\n current y:" + currentY);
+
+//                panel.getRaster().clear();
+//
+//                Point last = new Point(currentX, currentY);
+//
+//                if (polygon.getSize() == 1) {
+//                    LineRasterizer.rasterize(new Line(polygon.getPoint(0), last));
+//                } else if (polygon.getSize() == 2) {
+//                    LineRasterizer.rasterize(new Line(polygon.getPoint(0), polygon.getPoint(1)));
+//                    LineRasterizer.rasterize(new Line(polygon.getPoint(1), last));
+//                    LineRasterizer.rasterize(new Line(polygon.getPoint(0), last));
+//                } else if (polygon.getSize() >= 3) {
+//                    return;
+//                }
+//
+                panel.repaint();
             }
         });
 
@@ -179,14 +207,20 @@ public class Controller2D {
 
                 if (keyCode == KeyEvent.VK_SHIFT) {
                     shiftPressed = true;
-                } else if (keyCode == KeyEvent.VK_E) {
-                    polygon.addPoint(new Point(currentX, currentY));
-                    drawScene();
                 } else if (keyCode == KeyEvent.VK_C) {
-                    panel.getRaster().clear();
-                    panel.repaint();
                     lines.clear();
+                    rectangleDrawn = false;
+                    clippedPoints.clear();
+
+                    rectangle = new Rectangle();
+                    polygonClipper = new Polygon();
+                    polygonToDraw = new Polygon();
                     polygon = new Polygon();
+                    rPoints = new ArrayList<>(3);
+                    scanLineFiller = new ScanLineFiller(LineRasterizer);
+
+                    drawScene();
+
                 } else if (keyCode == KeyEvent.VK_V) {
                     LineRasterizer = new LineRasterizerTrivialColor(panel.getRaster(), color);
                 } else if (keyCode == KeyEvent.VK_F) {
@@ -194,21 +228,32 @@ public class Controller2D {
                         Filler currentFiller = new ScanLineFiller(
                                 polygon,
                                 panel.getRaster(),
-                                LineRasterizer // Použijeme existující rasterizér
+                                LineRasterizer
                         );
                         currentFiller.fill();
                     }
                     panel.repaint();
-                } else if (keyCode == KeyEvent.VK_Q) {
-                    polygonClipper.addPoint(new Point(currentX, currentY));
-                    drawScene();
-                }
+                } else if (keyCode == KeyEvent.VK_R) {
+                    if (rPoints.size() < 3) {
+                        rPoints.add(new Point(currentX, currentY));
+                    }
 
+                    if ((rPoints.size() == 3) && rectangleDrawn) {
+                        rPoints.clear();
+                        rectangle = new Rectangle();
+                        drawScene();
+                        panel.repaint();
+                        rectangleDrawn = false;
+                        rPoints.add(new Point(currentX, currentY));
+                    } else if (rPoints.size() == 3) {
+                        rectangle = new Rectangle(rPoints.getFirst(), rPoints.get(1), rPoints.get(2));
+                        drawScene();
+                    }
+                }
             }
 
             @Override
             public void keyReleased(KeyEvent e) {
-                // *** NOVÁ LOGIKA PRO SHIFT ***
                 if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
                     shiftPressed = false;
                 }
